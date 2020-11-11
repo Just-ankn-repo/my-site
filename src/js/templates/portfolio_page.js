aca428f0a0a9f470dfe55d0d95c46a52ff39b472
Andrei Knyshenko <andrei.knyshenko@gmail.com>
2020-11-11 12:13:19 +0400

    feat: change design for portfolio page

--- src/js/templates/portfolio_page.js
+++ src/js/templates/portfolio_page.js
@@ -3,11 +3,21 @@ import portfolioCardTpl from './portfolio_card';
 
 export default function (data) {
   return `
-    <section class="portfolio" id="portfolio">
-      <h2>Portfolio</h2>
-      <div class="portfolio__content">
+    <section class="portfolio">
+      <div class="portfolio__container">
+        <ul class="portfolio__filter">
+          <li class="portfolio__filter-item" tag="">All</li>
+          <li class="portfolio__filter-item" tag="javascript">JavaScript</li>
+          <li class="portfolio__filter-item" tag="html">HTML</li>
+          <li class="portfolio__filter-item" tag="css">CSS</li>
+          <li class="portfolio__filter-item" tag="angular">Angular</li>
+        </ul>
         <ul class="portfolio__list">
-          ${data.map((element, index) => `<li class="portfolio-card__wrapper" index="${index}">${portfolioCardTpl(element)}</li>`).join('')}
+          ${data.map((element) => `
+            <li class="portfolio__card" tags="${element.tags.join(',')}">
+              ${portfolioCardTpl(element)}
+            </li>
+          `).join('')}
         </ul>
       </div>
     </section>
